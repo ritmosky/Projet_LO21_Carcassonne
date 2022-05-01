923c97619a4cfabd6d046c3a4b3a6e171f5bd713
Ambroise THIBAULT <92878091+AmbroiseTHIBAULT@users.noreply.github.com>
2022-05-01 15:54:13 +0200

    ajout vueTest pour tester Qt

---
+++ src/vue/vueTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ambroise Thibault on 01/05/2022.
+//
+
+#include "vueTest.h"
---
+++ src/vue/vueTest.h
@@ -0,0 +1,8 @@
+//
+// Created by Ambroise Thibault on 01/05/2022.
+//
+
+#ifndef SRC_VUETEST_H
+#define SRC_VUETEST_H
+
+#endif //SRC_VUETEST_H
