0ebed32ff2b9b39ab6c8e77441e0e545fe972695
Ambroise THIBAULT <92878091+AmbroiseTHIBAULT@users.noreply.github.com>
2022-05-11 10:34:35 +0200

    pro

--- src/carcassone.pro
+++ src/carcassone.pro
@@ -2,12 +2,7 @@ QT += widgets
 
 SOURCES += \
     main.cpp \
-    modele/set.cpp \
-    vue/vuecarte.cpp \
-    vue/vuepartie.cpp
 
 
 HEADERS += \
     modele/set.h \
-    vue/vuecarte.h \
-    vue/vuepartie.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include <QApplication>
-#include "vue/vuepartie.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-
-    VuePartie partie;
-    partie.show();
-
-    return app.exec();
 
 }
