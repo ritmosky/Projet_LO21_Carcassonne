e9be0648c755f022d438a52b1b40f32eb2138d11
1Sylv1 <87544867+1Sylv1@users.noreply.github.com>
2022-06-10 22:47:11 +0200

    Update controller.cpp

--- src/modele/controller.cpp
+++ src/modele/controller.cpp
@@ -42,15 +42,31 @@
         modeJeu[4] = new AubergesEtCathedrales();
     }
 
-    void Controller::placementMeeple(Joueur* j,Meeple* m,TypeMeeple tm,int i,int x,int y){
-        Tuile *t = plateau->existeTuile(x,y);
-        ContenanceTuile contenance = t->getContenance(i);
-        m->setContenance(contenance);
-        m->setIdJoueur(j->getId());
-        m->setType(tm);
-        cout << *m << endl;
-        j->removeMeeple();
-    }
+    void Controller::placementMeeple(Joueur* j,Meeple* m,TypesTuiles tm,int i,int x,int y){
+       Tuile *t = plateau->existeTuile(x,y);
+       ContenanceTuile contenance = t->getContenance(i);
+       m->setContenance(contenance);
+       m->setIdJoueur(j->getId());
+       if(tm==TypesTuiles::champs){
+           m->setType(NomMeeple::paysan);
+       }
+       else if(tm==TypesTuiles::abbaye || tm==TypesTuiles::jardin){
+           m->setType(NomMeeple::abbe);
+       }
+       else if(tm==TypesTuiles::ville){
+           m->setType(NomMeeple::chevalier);
+       }
+       else if(tm==TypesTuiles::route){
+           m->setType(NomMeeple::voleur);
+       }
+       else if(tm==TypesTuiles::champs){
+           m->setType(NomMeeple::paysan);
+       }
+       else{
+           m->setType(NomMeeple::rien);
+       }
+       j->removeMeeple();
+   }
 
     void Controller::placementTuile(Tuile *newTuile,int x,int y){
         newTuile->setPosX(x);
@@ -190,277 +206,6 @@
         }
         else{
             tour++;
-        }
-    }
-
-
-/* ************************************* TEST**************************************************** */
-
-         // test placement tuile avec plateau
-        bool Controller::placementTuileAutorise(Tuile newTuile,Plateau *plateau){
-        //Parcourir toutes les tuiles du plateau
-        for(Tuile *tuiles5 : plateau->getTuiles()){
-            if(tuiles5->getVoisinHaut() == NULL){
-                for(int rotation=0;rotation<4; rotation++){
-                    if(estCompatible(newTuile,tuiles5->getX(),tuiles5->getY()-1,plateau)){
-                        cout << " PLACEMENT DU HAUT AUTORISE " << endl;
-                        return true;
-                    }
-                    newTuile.changerOrientation();
-                }
-            }
-               cout << " voisin du haut fait" << endl;
-
-            //Voisin du bas
-            if(tuiles5->getVoisinBas() == NULL){
-                for(int rotation=0;rotation<4; rotation++){
-                    if(estCompatible(newTuile, tuiles5->getX(),tuiles5->getY()+1,plateau)){
-                        cout << " PLACEMENT DU BAS AUTORISE " << endl;
-                        return true;
-                    }
-                    newTuile.changerOrientation();
-                }
-            }
-            cout << " voisin du bas fait" << endl;
-
-            //Voisin de gauche
-            if(tuiles5->getVoisinGauche() == NULL){
-                for(int rotation=0;rotation<4; rotation++){
-                    if(estCompatible(newTuile, tuiles5->getX()-1,tuiles5->getY(),plateau)){
-                        cout << " PLACEMENT DE GAUCHE AUTORISE " << endl;
-                        return true;
-                    }
-                    newTuile.changerOrientation();
-                }
-            }
-               cout << " voisin de gauche fait" << endl;
-            //Voisin de droite
-            if(tuiles5->getVoisinDroite() == NULL){
-                for(int rotation=0;rotation<4; rotation++){
-                    if(estCompatible(newTuile, tuiles5->getX()+1,tuiles5->getY(),plateau)){
-                        cout << " PLACEMENT DE DROITE AUTORISE " << endl;
-                        return true;
-                    }
-                    newTuile.changerOrientation();
-                }
-            }
-        }
-           cout << "voisin de droite fait" << endl;
-
-    return false;
-    }
-
-
-     //test compatible avec plateau
-     bool Controller::estCompatible(Tuile newTuile,int x,int y,Plateau *plateau){
-        if(plateau->existeTuile(x,y-1)){
-            if(newTuile.getContenu(1)!=plateau->existeTuile(x,y-1)->getContenu(5)){
-                return false;
-            }
-        }
-
-
-        //Voisin du bas
-
-        if(plateau->existeTuile(x,y+1)){
-            if(newTuile.getContenu(5)!=plateau->existeTuile(x,y+1)->getContenu(1)){
-                return false;
-            }
-        }
-
-        //Voisin de gauche
-        if(plateau->existeTuile(x-1,y)){
-            if(newTuile.getContenu(7)!=plateau->existeTuile(x-1,y)->getContenu(3)){
-                return false;
-            }
-        }
-
-        //Voisin de droite
-        if(plateau->existeTuile(x+1,y)){
-            if(newTuile.getContenu(3)!=plateau->existeTuile(x+1,y)->getContenu(7)){
-                return false;
-            }
-        }
-
-        return true;
-
-    }
-
-
-
-
-/*
-void Controller::Abbaye(State s) {
-        int score = 1;
-        for (Tuile* tuile : plateau->getTuiles()) {
-            TypesTuiles const typetuile = tuile->getContenu(8);
-            ContenanceTuile contenance = tuile->getContenance(8);
-            if (typetuile == TypesTuiles::abbaye || contenance.hasMeeple()) {
-                // HAUT GAUCHE
-                if ((tuile->getVoisinHaut()->getVoisinGauche())!= NULL) {
-                    score++;
-                }
-
-                // HAUT
-                if ((tuile->getVoisinHaut()) != NULL) {
-                    score++;
-                }
-
-                // HAUT DROITE
-                if ((tuile->getVoisinHaut()->getVoisinDroite()) != NULL) {
-                    score++;
-                }
-
-                // GAUCHE
-                if ((tuile->getVoisinGauche()) != NULL) {
-                    score++;
-                }
-
-                // DROITE
-                if ((tuile->getVoisinDroite()) != NULL) {
-                    score++;
-                }
-
-                // BAS GAUCHE
-                if ((tuile->getVoisinBas()->getVoisinGauche())!= NULL) {
-                    score++;
-                }
-
-                // BAS DROITE
-                if ((tuile->getVoisinBas()->getVoisinDroite()) != NULL) {
-                    score++;
-                }
-
-                // BAS
-                if ((tuile->getVoisinBas()) != NULL) {
-                    score++;
-                }
-
-                if (score == 9 || s == GAME_OVER) {
-                    contenance.getJoueur().addScore(score);
-                }
-            }
-            score = 1;
-        }
-    }
-
-    */
-
-        /*
-    //Comptage des scores
-    void Controller::compteScore(State state){
-        if (state == GAME_OVER){
-           compteScore(TypesTuiles::champs, state);
-        }
-        compteScore(TypesTuiles::ville, state);
-        compteScore(TypesTuiles::route, state); // à implémenter une méthode compte score pour les routes et villes
-        compteScoreAbbaye(state);
-    }
-*/
-
-bool Controller::validationPlacementRiviere(Tuile newTuile,int x,int y,Plateau *plateau){
-
-        //Voisin du haut
-        if(plateau->existeTuile(x,y-1)){
-            cout << " JE PASSE ICI " << endl;
-            if(newTuile.getContenu(1)!=plateau->existeTuile(x,y-1)->getContenu(5)
-            || (newTuile.getContenu(1)==plateau->existeTuile(x,y-1)->getContenu(5)
-                && newTuile.getContenu(3)==plateau->existeTuile(x,y-1)->getContenu(3)
-                && newTuile.getContenu(3)==TypesTuiles::rivière
-                && newTuile.getContenu(1)==TypesTuiles::rivière)
-            || (newTuile.getContenu(1)==plateau->existeTuile(x,y-1)->getContenu(5)
-                && newTuile.getContenu(7)==plateau->existeTuile(x,y-1)->getContenu(7)
-                && newTuile.getContenu(7)==TypesTuiles::rivière
-                && newTuile.getContenu(1)==TypesTuiles::rivière)){
-                return false;
-            }
-        }
-
-        //Voisin du bas
-
-        if(plateau->existeTuile(x,y+1)){
-            cout << " JE PASSE ICI " << endl;
-            if(newTuile.getContenu(5)!=plateau->existeTuile(x,y+1)->getContenu(1)
-            || (newTuile.getContenu(5)==plateau->existeTuile(x,y+1)->getContenu(1)
-                && newTuile.getContenu(3)==plateau->existeTuile(x,y+1)->getContenu(3)
-                && newTuile.getContenu(5)==TypesTuiles::rivière
-                && newTuile.getContenu(7)==TypesTuiles::rivière)
-            || (newTuile.getContenu(5)==plateau->existeTuile(x,y+1)->getContenu(1)
-                && newTuile.getContenu(7)==plateau->existeTuile(x,y+1)->getContenu(7)
-                && newTuile.getContenu(5)==TypesTuiles::rivière
-                && newTuile.getContenu(3)==TypesTuiles::rivière)){
-                return false;
-            }
-        }
-
-        //Voisin de gauche
-        if(plateau->existeTuile(x-1,y)){
-            if(newTuile.getContenu(7)!=plateau->existeTuile(x-1,y)->getContenu(3)
-            || (newTuile.getContenu(7)==plateau->existeTuile(x-1,y)->getContenu(3)
-                && newTuile.getContenu(1)==plateau->existeTuile(x-1,y)->getContenu(1)
-                && newTuile.getContenu(7)==TypesTuiles::rivière
-                && newTuile.getContenu(1)==TypesTuiles::rivière)
-            || (newTuile.getContenu(7)==plateau->existeTuile(x-1,y)->getContenu(3)
-                && newTuile.getContenu(5)==plateau->existeTuile(x-1,y)->getContenu(5)
-                && newTuile.getContenu(5)==TypesTuiles::rivière
-                && newTuile.getContenu(7)==TypesTuiles::rivière)){
-                return false;
-            }
-        }
-
-        //Voisin de droite
-        if(plateau->existeTuile(x+1,y)){
-            cout << " JE PASSE ICI " << endl;
-            if(newTuile.getContenu(3)!=plateau->existeTuile(x+1,y)->getContenu(7)
-            || (newTuile.getContenu(3)==plateau->existeTuile(x+1,y)->getContenu(7)
-                && newTuile.getContenu(1)==plateau->existeTuile(x+1,y)->getContenu(1)
-                && newTuile.getContenu(3)==TypesTuiles::rivière
-                && newTuile.getContenu(1)==TypesTuiles::rivière)
-            || (newTuile.getContenu(3)==plateau->existeTuile(x+1,y)->getContenu(7)
-                && newTuile.getContenu(5)==plateau->existeTuile(x+1,y)->getContenu(5)
-                && newTuile.getContenu(3)==TypesTuiles::rivière
-                && newTuile.getContenu(5)==TypesTuiles::rivière)){
-                return false;
-            }
-        }
-
-        return true;
-
-    }
-
-     void Controller::placementMeeple(Joueur* j,Meeple* m,TypeMeeple tm,int i,int x,int y,Plateau *plateau){
-        Tuile *t = plateau->existeTuile(x,y);
-        ContenanceTuile contenance = t->getContenance(i);
-        m->setContenance(contenance);
-        m->setIdJoueur(j->getId());
-        m->setType(tm);
-        cout << *m << endl;
-        j->removeMeeple();
-    }
-
-    void Controller::placementTuile(Tuile *newTuile,int x,int y,Plateau *plateau){
-        newTuile->setPosX(x);
-        newTuile->setPosY(y);
-        plateau->ajouterTuiles(newTuile);
-        //Voisin du haut
-        if(plateau->existeTuile(x,y-1)){
-            newTuile->setVoisinHaut(plateau->existeTuile(x,y-1));
-            plateau->existeTuile(x,y-1)->setVoisinBas(newTuile);
-        }
-        //Voisin du bas
-        if(plateau->existeTuile(x,y+1)){
-            newTuile->setVoisinBas(plateau->existeTuile(x,y+1));
-            plateau->existeTuile(x,y+1)->setVoisinHaut(newTuile);
-        }
-        //Voisin de gauche
-        if(plateau->existeTuile(x-1,y)){
-            newTuile->setVoisinGauche(plateau->existeTuile(x-1,y));
-            plateau->existeTuile(x-1,y)->setVoisinDroite(newTuile);
-        }
-        //Voisin de droite
-        if(plateau->existeTuile(x+1,y)){
-            newTuile->setVoisinDroite(plateau->existeTuile(x+1,y));
-            plateau->existeTuile(x+1,y)->setVoisinGauche(newTuile);
         }
     }
 
